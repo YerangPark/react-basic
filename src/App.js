7c14d210db3b4757afc9547b90378be9f7aa08f2
yerang2zzang <buuuuung@naver.com>
2023-11-05 15:59:47 +0900

    Completed until 7th lecture

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import Hello from './component/Hello';
-import Welcome from './component/Welcome';
-import styles from './App.module.css';
 
 function App() {
     return <div className="App">
         <Hello />
-        <div className={styles.box}>.css in App Comp</div>
+        <Hello />
+        <Hello />
     </div>;
 }
 
--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,10 +1,16 @@
-import World from './World';
-import styles from './Hello.module.css';
+import { useState } from "react";
 
 export default function Hello() {
+    const [name, setName] = useState("Mike");
+     
+    function changeName() {
+        setName((name === "Mike") ? "Jane" : "Mike");
+    }
     return <div>
-        <p>Hello</p>
-        <World />
-        <div className={styles.box}>.css in Hello Comp</div>
+        <h1>State</h1>
+        <h2 id="name">{name}</h2> 
+        <button onClick={()=>{
+            setName((name === "Mike") ? "Jane" : "Mike");
+        }}>Change Name</button>
     </div>;
 }
