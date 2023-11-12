b8e749fb3b930df4219e179418d0c8bf0d82d32a
yerang2zzang <buuuuung@naver.com>
2023-11-12 15:24:08 +0900

    lecture 8

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Hello from './component/Hello';
 
 function App() {
     return <div className="App">
-        <Hello />
-        <Hello />
-        <Hello />
+        <h1>Props : properties</h1>
+        <Hello age={10}/>
+        <Hello age={20}/>
+        <Hello age={30}/>
     </div>;
 }
+// 프로퍼티로 준 값이 Hello 컴포넌트에 인자로 전달됨.
 
 export default App;
